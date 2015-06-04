561e5d2353d68cc875e3c7de20e9fffbdba8ef9d
TomWan <wanmingtom@gmail.com>
2015-06-04 20:01:53 +0800

    add time zone support

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ var email   = require("emailjs/email");
 var chalk = require('chalk');
 var fs = require('fs');
 var path = require('path');
-var moment = require('moment');
+var moment = require('moment-timezone');
 
 try {
   settings = require('./config');
@@ -17,6 +17,7 @@ try {
 }
 
 var slice = [].slice;
+var now = undefined;
 
 var logPath = path.join(process.env.PWD, settings.log);
 var emailSender = email.server.connect(settings.sender);
@@ -26,12 +27,14 @@ try {
   main();
 } catch (ex) {
   fatalErrorHandle(ex);
+  log(ex.stack || ex);
 }
 
 function main () {
 
   var job = new CronJob(
     '*/30 * 9-14 * * 1-5',
+    // '*/30 * * * * 1-5', // for test
     doJob,
     function () {},
     true,
@@ -57,16 +60,16 @@ function processDataAndSendEmail (data, callback) {
 
   data.list.forEach(function (item) {
 
-    if (+new Date - item.updated_at < 60 * 60 * 1000 && !visited[item.id]) {
+    if (+new Date - item.updated_at < 10 * 60 * 1000 && !visited[item.id]) {
 
       var title = _.template(settings.template.title)({
-        stock_name: data.stock_name
+        stock_name: data.stock_name + ' ' + formatTime(item.updated_at, 'HH:mm')
       });
       var content = _.template(settings.template.content)({
         stock_symbol: data.stock_symbol,
         stock_name: data.stock_name,
         html_url: data.html_url,
-        updated_at: moment(item.updated_at).format('YYYY-MM-DD HH:mm:ss'),
+        updated_at: formatTime(item.updated_at, 'YYYY-MM-DD HH:mm:ss'),
         list: item.rebalancing_histories
       });
 
@@ -145,7 +148,7 @@ function fatalErrorHandle (err) {
 
   error(JSON.stringify(err.stack || err));
 
-  if (!lastSendErrorEmailTime || +new Date - lastSendErrorEmailTime > 60 * 60 * 1000) {
+  if (!lastSendErrorEmailTime || +new Date - lastSendErrorEmailTime > 20 * 60 * 1000) {
 
     sendEmail([ settings.errorEmail ], 'FETCH_XQ_DATA_ERROR.', 'RT');
     lastSendErrorEmailTime = +new Date;
@@ -160,7 +163,7 @@ function konsole (prefix, style) {
 
   var content = prefix + args.join(' ');
 
-  fs.appendFileSync(logPath, '[' + moment().format('YYYY-MM-DD HH:mm:ss') + '] ' + content + '\n');
+  fs.appendFileSync(logPath, '[' + formatTime(now, 'YYYY-MM-DD HH:mm:ss') + '] ' + content + '\n');
 
   console.log.call(null, style ? style(content) : content);
 }
@@ -175,4 +178,8 @@ function warn () {
 
 function error () {
   konsole.apply(null, ['ERROR:', chalk.red].concat(slice.apply(arguments)));
+}
+
+function formatTime (time, format) {
+  return moment(time).tz('Asia/Shanghai').format(format);
 }
